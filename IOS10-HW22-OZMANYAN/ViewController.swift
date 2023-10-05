7378d22f28ffef0a242046d7518417c2650098f9
Amo Ozmanyan <cool.ozmanyan2013@yandex.ru>
2023-10-06 01:25:03 +0800

    Initial Commit

---
+++ IOS10-HW22-OZMANYAN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS10-HW22-OZMANYAN
+//
+//  Created by Amahastla on 06.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
