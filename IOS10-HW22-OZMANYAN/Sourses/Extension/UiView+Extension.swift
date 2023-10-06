88e498d6a5b9c00b2cc070c7d6e58bc940e4a68d
Amo Ozmanyan <cool.ozmanyan2013@yandex.ru>
2023-10-06 22:38:46 +0800

    Revision of errors.

--- IOS10-HW22-OZMANYAN/Sourses/Extension/UiView+Extension.swift
+++ IOS10-HW22-OZMANYAN/Sourses/Extension/UiView+Extension.swift
@@ -5,4 +5,17 @@
 //  Created by Amahastla on 06.10.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    func addSubviews(_ views: UIView...) {
+        views.forEach { self.addSubview($0) }
+    }
+}
+
+extension UIView {
+    func addTapGestureToHideKeyboard() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(endEditing))
+        addGestureRecognizer(tapGesture)
+    }
+}
